30063fb3f198735ab3a352054ac1fa34093f61e6
achantatejaswi <achantatejaswiteju@gmail.com>
2026-09-11 19:29:28 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.73%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int insertPos = 0;
+        for (int i = 0; i<n; i++) {
+            if (nums[i] != 0) {
+                nums[insertPos] =nums[i];
+                insertPos++;
+            }
+        }
+        for (int i = insertPos; i<n; i++) {
+            nums[i] = 0;
+        }
+    }
+};
