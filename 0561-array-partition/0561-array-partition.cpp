d916138f24da57d655b09cb0a3cf4d4bb116b31c
achantatejaswi <achantatejaswiteju@gmail.com>
2026-09-17 21:04:47 +0530

    Time: 8 ms (68.38%), Space: 32.1 MB (20.79%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        int total_sum = 0;
+        for (int i = 0; i< nums.size(); i +=2) {
+            total_sum += nums[i];
+        }
+        return total_sum;
+    }
+};
+    
